d2fc9a57a9ae1eba8b1aecd5ecd1d4e2c5c65369
Thanh Hoang <thhoang@pdx.edu>
2017-04-04 23:25:53 -0700

    add: take quiz submenu

--- app/src/main/java/com/psu/capstonew17/pdxaslapp/TakeQuizSubMenuActivity.java
+++ app/src/main/java/com/psu/capstonew17/pdxaslapp/TakeQuizSubMenuActivity.java
@@ -1,19 +1,55 @@
 package com.psu.capstonew17.pdxaslapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class TakeQuizSubMenuActivity extends BaseActivity implements View.OnClickListener {
+    private Button bttMultipleChoice, bttFlashCard, bttWRiteUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_quiz_submenu);
+
+        bttMultipleChoice = (Button) this.findViewById(R.id.button_multiple_choice);
+        bttMultipleChoice.setOnClickListener(this);
+
+        bttFlashCard = (Button) this.findViewById(R.id.button_flash_card);
+        bttFlashCard.setOnClickListener(this);
+
+        bttWRiteUp = (Button) this.findViewById(R.id.button_write_up);
+        bttWRiteUp.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        Intent intent;
+
+        switch(view.getId()) {
+
+            case R.id.button_multiple_choice:
+                intent = new Intent(getApplicationContext(), MultipleChoiceActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+
+            case R.id.button_flash_card:
+                intent = new Intent(getApplicationContext(), FlashCardActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+
+            case R.id.button_write_up:
+                intent = new Intent(getApplicationContext(), WriteUpActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+
+        }
 
     }
 }
