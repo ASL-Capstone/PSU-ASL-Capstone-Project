6424886a3cc9478d533d8bccf2e11c3abc256de6
Thanh Hoang <thhoang@pdx.edu>
2017-04-04 18:26:08 -0700

    remake layout for multiple choice

---
+++ app/src/main/java/com/psu/capstonew17/pdxaslapp/MultipleChoiceActivity.java
@@ -0,0 +1,22 @@
+package com.psu.capstonew17.pdxaslapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by thanhhoang on 4/4/17.
+ */
+
+public class MultipleChoiceActivity extends BaseActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/psu/capstonew17/pdxaslapp/WriteUpActivity.java
@@ -0,0 +1,26 @@
+package com.psu.capstonew17.pdxaslapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+/**
+ * Created by thanhhoang on 4/4/17.
+ */
+
+public class WriteUpActivity extends BaseActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_write_up);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
