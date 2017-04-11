984a6ec0a4dba59868c68a2e1b0a9a34a6b4e168
Thanh Hoang <thhoang@pdx.edu>
2017-04-11 12:31:59 -0700

    updated

--- app/src/main/java/com/psu/capstonew17/pdxaslapp/ShareDeckActivity.java
+++ app/src/main/java/com/psu/capstonew17/pdxaslapp/ShareDeckActivity.java
@@ -8,6 +8,6 @@ public class ShareDeckActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_share_deck);
+        setContentView(R.layout.share);
     }
 }
