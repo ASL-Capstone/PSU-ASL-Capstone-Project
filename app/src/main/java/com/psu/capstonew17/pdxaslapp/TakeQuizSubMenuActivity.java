c2c9f56cf639e301151da532814d8ac2a4952cba
Thanh Hoang <thhoang@pdx.edu>
2017-04-16 14:14:45 -0700

    Fixed: dropdown menu background

--- app/src/main/java/com/psu/capstonew17/pdxaslapp/TakeQuizSubMenuActivity.java
+++ app/src/main/java/com/psu/capstonew17/pdxaslapp/TakeQuizSubMenuActivity.java
@@ -20,41 +20,41 @@ public class TakeQuizSubMenuActivity extends BaseActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_quiz_submenu);
 
-        bttMultipleChoice = (Button) this.findViewById(R.id.button_multiple_choice);
-        bttMultipleChoice.setOnClickListener(this);
-
-        bttFlashCard = (Button) this.findViewById(R.id.button_flash_card);
-        bttFlashCard.setOnClickListener(this);
-
-        bttWRiteUp = (Button) this.findViewById(R.id.button_write_up);
-        bttWRiteUp.setOnClickListener(this);
+//        bttMultipleChoice = (Button) this.findViewById(R.id.button_multiple_choice);
+//        bttMultipleChoice.setOnClickListener(this);
+//
+//        bttFlashCard = (Button) this.findViewById(R.id.button_flash_card);
+//        bttFlashCard.setOnClickListener(this);
+//
+//        bttWRiteUp = (Button) this.findViewById(R.id.button_write_up);
+//        bttWRiteUp.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         Intent intent;
 
-        switch(view.getId()) {
-
-            case R.id.button_multiple_choice:
-                intent = new Intent(getApplicationContext(), MultipleChoiceActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-
-            case R.id.button_flash_card:
-                intent = new Intent(getApplicationContext(), FlashCardActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-
-            case R.id.button_write_up:
-                intent = new Intent(getApplicationContext(), WriteUpActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-
-        }
+//        switch(view.getId()) {
+//
+//            case R.id.button_multiple_choice:
+//                intent = new Intent(getApplicationContext(), MultipleChoiceActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//
+//            case R.id.button_flash_card:
+//                intent = new Intent(getApplicationContext(), FlashCardActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//
+//            case R.id.button_write_up:
+//                intent = new Intent(getApplicationContext(), WriteUpActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//
+//        }
 
     }
 }
