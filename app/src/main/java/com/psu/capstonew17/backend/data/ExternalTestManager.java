438db99a934a7cfcfdccd245ce8c61db929f19a1
Noah Zentzis <nzentzis@gmail.com>
2017-05-06 15:55:12 -0700

    Fix member visibility in external backend classes

--- app/src/main/java/com/psu/capstonew17/backend/data/ExternalTestManager.java
+++ app/src/main/java/com/psu/capstonew17/backend/data/ExternalTestManager.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class ExternalTestManager implements TestManager{
-    public static ExternalTestManager INSTANCE = new ExternalTestManager();
+    static ExternalTestManager INSTANCE = new ExternalTestManager();
 
     private AslDbHelper dbHelper;
 
@@ -23,7 +23,7 @@ public class ExternalTestManager implements TestManager{
         return INSTANCE;
     }
 
-    public AslDbHelper getDbHelper(){
+    AslDbHelper getDbHelper(){
         return dbHelper;
     }
 
