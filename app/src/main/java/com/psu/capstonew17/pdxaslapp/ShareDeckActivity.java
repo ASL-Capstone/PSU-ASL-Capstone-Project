5e0112a0e2f777c9fef45d77f8264e0f204a5b17
Brook Baise <brook2@pdx.edu>
2017-05-06 18:26:09 -0700

    Updates To Share Video

--- app/src/main/java/com/psu/capstonew17/pdxaslapp/ShareDeckActivity.java
+++ app/src/main/java/com/psu/capstonew17/pdxaslapp/ShareDeckActivity.java
@@ -1,14 +1,19 @@
 //MIT License Copyright 2017 PSU ASL Capstone Team
 package com.psu.capstonew17.pdxaslapp;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.psu.capstonew17.backend.api.Deck;
@@ -19,12 +24,19 @@ import com.psu.capstonew17.pdxaslapp.FrontEndTestStubs.TestingStubs;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShareDeckActivity extends BaseActivity implements View.OnClickListener {
+public class ShareDeckActivity extends BaseActivity implements View.OnClickListener{
     private Button submit;
     private LinearLayout ll;
     private RadioGroup rg;
     private List<Deck> decks;
-    private Bitmap qrCode;
+    private ScrollView sv;
+
+    //Popup Window Objects
+    private PopupWindow popupWindow;
+    private LinearLayout popupLL;
+    private ImageView imageView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +49,6 @@ public class ShareDeckActivity extends BaseActivity implements View.OnClickListe
 
         //TODO Database call should go here
         this.decks = TestingStubs.manyDecks();
-        //List<String> deckLabels = new ArrayList<String>();
 
         //Create Radio Buttons For Each Deck and add them to the RadioGroup
         int i = 0;
@@ -51,6 +62,8 @@ public class ShareDeckActivity extends BaseActivity implements View.OnClickListe
         ((ViewGroup)findViewById(R.id.deckList)).addView(this.rg);
         this.submit = (Button) findViewById(R.id.button3);
         this.submit.setOnClickListener(this);
+
+        this.sv = (ScrollView) findViewById(R.id.decksView);
     }
 
     @Override
@@ -61,25 +74,48 @@ public class ShareDeckActivity extends BaseActivity implements View.OnClickListe
                 if(id != -1){
                     Deck toShare = decks.get(id);
                     //TODO create popup window for displaying the QR code recieved from backend
-                    SharingTransmitListener listener;
+                    Intent intent = new Intent(this, DispQRCodeActivity.class);
+                    //toShare.getName();
+                    intent.putExtra("DECKNAME", toShare.getName());
+                    startActivity(intent);
+                    finish();
+                    return;
+                    /*
+                    toShare.getName();
                     List<Deck> sharing = new ArrayList<>();
                     sharing.add(toShare);
-                    //SharingManager sharer;
-                    //SharingManager.TxOptions ops = new SharingManager.TxOptions();
-                    //TODO Find out how these params are decided (SharingManager.TxOptions timeout, maxTargets)
-                    // Do they get default values? Do we ask the user?
-                    //ops.timeout = 360;
-                    //ops.maxTargets = 30;
-                    //qrCode = sharer.transmit(null, toShare,ops, listener);
+                    setUpPopupWindow(sharing);
+                    */
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "No Deck Selected", Toast.LENGTH_SHORT).show();
-                    //((TextView) findViewById(R.id.TESTTEXT)).setText("No Deck Selected");
                 }
-
                 break;
-
         }
+
+    }
+
+
+    public void setUpPopupWindow(List<Deck> sharing){
+
+
+        //sv.setVisibility(View.INVISIBLE);
+        //submit.setVisibility(View.INVISIBLE);
+/*        popupWindow = new PopupWindow(this);
+        popupWindow.showAtLocation(ll, Gravity.BOTTOM, 10, 10);
+        popupWindow.update(50,50,320,90);
+        popupWindow.showAtLocation(ll, Gravity.BOTTOM, 10, 10);*/
+        /*
+        popupLL = new LinearLayout(this);
+        popupLL.setOrientation(LinearLayout.VERTICAL);
+        imageView = new ImageView(this);
+        imageView.setImageBitmap(qrCode);
+        popupLL.addView(imageView);
+        popupWindow.setContentView(popupLL);
+        */
+
+
+
 
     }
 }
