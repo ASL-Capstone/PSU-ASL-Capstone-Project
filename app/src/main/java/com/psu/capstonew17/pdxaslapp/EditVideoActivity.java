169440b7cef4e5749c3fb5cdeba3abc86fbd8c69
Josh Aldridge <takealookphoto@gmail.com>
2017-06-07 15:29:47 -0700

    EditCard will make video 30 seconds if it's too long

--- app/src/main/java/com/psu/capstonew17/pdxaslapp/EditVideoActivity.java
+++ app/src/main/java/com/psu/capstonew17/pdxaslapp/EditVideoActivity.java
@@ -303,10 +303,12 @@ public class EditVideoActivity extends BaseActivity implements View.OnClickListe
          * If the video is an invalid length
          */
         if(!timeSelectionCheck(importOptions.startTime, importOptions.endTime)) {
+            //if the video is less than 30 seconds
             if(videoView.getDuration() <= 30000) {
                 importOptions.startTime = 0; //zero
                 importOptions.endTime = videoView.getDuration(); //to the length of the video
             } else {
+                //if the video is LONGER than 30 seconds
                 importOptions.startTime = 0; //zero
                 importOptions.endTime = 30000; //to the max length
             }
